949eab1f939e857fb84369e207b5a9bc238041a7
AJ-SR <cubscoutaj@gmail.com>
2015-10-27 11:25:43 -0500

    Update ItemSpaceCowEgg.java

--- src/main/java/ajsr/ProjectClock/items/ItemSpaceCowEgg.java
+++ src/main/java/ajsr/ProjectClock/items/ItemSpaceCowEgg.java
@@ -1,1 +1,16 @@
+package ajsr.ProjectClock.items
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemSpaceCowEgg extends Item
+{
+  @Override
+	@SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack stack)
+    {
+        return true;
+    }
+}
